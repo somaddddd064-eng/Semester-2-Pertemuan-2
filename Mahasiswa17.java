525e27941efe6481b8de73003dfbbeed79c2d76d
somaddddd064-eng <somaddddd064@gmail.com>
2026-02-19 11:34:23 +0700

    2.1.1

---
+++ Mahasiswa17.java
@@ -0,0 +1,38 @@
+public class Mahasiswa17 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInnformasi() {
+        System.out.println("Nama = " + nama);
+        System.out.println("NIM = " + nim);
+        System.out.println("Kelas = " + kelas);
+        System.out.println("IPK = " + ipk);
+    }
+    
+    void ubahKelas (String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk (double ipkBaru) {
+        ipk = ipkBaru;
+        if (ipk >= 0.0 && ipk <= 4.0){
+
+        }else {
+            System.out.println("GPA is invalid, must be between 0.0 and 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
